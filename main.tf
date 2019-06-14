16795a3470ec38a16d2c48656bc270c73000dd1c
Quentin Vallin <quentin.vallin@fxinnovation.com>
2019-06-14 10:28:00 -0400

    add write output

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ data "aws_iam_policy_document" "read" {
       "ssm:GetParameters",
     ]
 
-    resources = ["${formatlist("arn:aws:ssm:*:%s:parameter/%s/%s", data.aws_caller_identity.current.account_id, var.prefix, var.names)}"]
+    resources = ["${formatlist("arn:aws:ssm:*:%s:parameter/%s%s", data.aws_caller_identity.current.account_id, var.prefix, var.names)}"]
   }
 
   statement {
@@ -77,10 +77,53 @@ data "aws_iam_policy_document" "read" {
   }
 }
 
+data "aws_iam_policy_document" "read_write" {
+  statement {
+    sid = "Allow${var.prefix}SSMParameterAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "ssm:DescribeAssociation",
+      "ssm:GetDocument",
+      "ssm:DescribeDocument",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:PutParameter",
+    ]
+
+    resources = ["${formatlist("arn:aws:ssm:*:%s:parameter/%s%s", data.aws_caller_identity.current.account_id, var.prefix, var.names)}"]
+  }
+
+  statement {
+    sid = "Allow${var.prefix}SSMParameterKMSAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:ListKeyPolicies",
+      "kms:GetKeyPolicy",
+      "kms:DescribeKey",
+    ]
+
+    resources = ["${var.kms_key_create ? element(concat(aws_kms_key.this.*.arn, list("")), 0) : var.kms_key_arn}"]
+  }
+}
+
 resource "aws_iam_policy" "read" {
   count       = "${var.enabled && var.iam_policy_create ? 1 : 0}"
   name_prefix = "${var.iam_policy_name_prefix_read_only}"
   path        = "${var.iam_policy_path}"
   policy      = "${data.aws_iam_policy_document.read.json}"
   description = "Read only policy to get access to ${var.prefix} SSM parameters."
+}
+
+resource "aws_iam_policy" "read_write" {
+  count       = "${var.enabled && var.iam_policy_create ? 1 : 0}"
+  name_prefix = "${var.iam_policy_name_prefix_read_write}"
+  path        = "${var.iam_policy_path}"
+  policy      = "${data.aws_iam_policy_document.read_write.json}"
+  description = "Read write policy to get access to ${var.prefix} SSM parameters."
 }
--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,13 @@ output "iam_policy_read_only_path" {
   description = "Path of the read only policy"
   value       = "${element(concat(aws_iam_policy.read.*.path, list("")), 0)}"
 }
+
+output "iam_policy_read_write_arn" {
+  description = "ARN of the read write policy"
+  value       = "${element(concat(aws_iam_policy.read_write.*.arn, list("")), 0)}"
+}
+
+output "iam_policy_read_write_path" {
+  description = "Path of the read write policy"
+  value       = "${element(concat(aws_iam_policy.read_write.*.path, list("")), 0)}"
+}
