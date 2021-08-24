22bf25d160996f954428afb1384b9964c9021891
Charles CEBE <charles.cebe@fxinnovation.com>
2021-08-24 14:07:28 -0400

    chore: bumps terraform + providers versions in examples
    * chore: pins terraform to >= 0.14
    * chore: pins aws provider to >= 3.5
    * chore: pins random provider to >= 3.1

--- examples/external_kms_no_policy/versions.tf
+++ examples/external_kms_no_policy/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 3.5"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0"
+      version = ">= 3.1"
     }
   }
 }
