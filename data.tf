ea2545d1b2d859ea7c7df1232776183c341e62b1
Charles CEBE <charles.cebe@fxinnovation.com>
2021-08-30 18:39:52 -0400

    refactor: lint data.tf

--- data.tf
+++ data.tf
@@ -1,5 +1,4 @@
-data "aws_caller_identity" "current" {
-}
+data "aws_caller_identity" "current" {}
 
 locals {
   kms_key_needed = (var.kms_key_create || contains(var.types, "SecureString")) && !var.use_default_kms_key
