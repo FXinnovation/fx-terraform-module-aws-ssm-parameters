1bf146ff17b0e56bb77833ec287981b81df02e93
Charles CEBE <charles.cebe@fxinnovation.com>
2021-09-07 13:52:06 -0400

    fix: least provider versions

--- examples/no_kms/versions.tf
+++ examples/no_kms/versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.5"
+      version = ">= 3.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.1"
+      version = ">= 3.0"
     }
   }
 }
